4600bf2660e090ec153fd4e2a1f9f2d91546455c
Samridhhi Sharma <sharma.samridhhi9@gmail.com>
2024-10-11 08:24:33 +0530

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTHDOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECTID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGEBUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGINGSENDERID,
+  appId: process.env.REACT_APP_FIREBASE_APPID,
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth();
--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,9 +1,32 @@
-import "./login.scss"
+import { useState } from "react";
+import "./login.scss";
+import { createUserWithEmailAndPassword } from "firebase/auth";
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+  const [error, setError] = useState(false);
 
-export default Login
+  const handleLogin = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div className='login'>
+      <form onSubmit={handleLogin}>
+        <input
+          type='email'
+          placeholder='email'
+          // onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type='password'
+          placeholder='password'
+          // onChange={(e) => setPassword(e.target.value)}
+        />
+        <button type='submit'>Login</button>
+        {error && <span>Wrong email or password!</span>}
+      </form>
+    </div>
+  );
+};
+
+export default Login;
