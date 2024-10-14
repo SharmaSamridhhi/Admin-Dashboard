caa5581fc173ad73934cc623f5d1f34724f42100
Samridhhi Sharma <sharma.samridhhi9@gmail.com>
2024-10-14 22:43:38 +0530

    first commit

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,8 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTHDOMAIN,
@@ -10,4 +13,6 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
 export const auth = getAuth();
+export const storage = getStorage(app);
